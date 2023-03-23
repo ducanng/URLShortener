6d97cac2c6fc07ff713af0dc1e668794a0720258
An Nguyen <ducan172002@gmail.com>
2023-03-24 00:47:26 +0700

    render UI

--- router/router.go
+++ router/router.go
@@ -5,14 +5,23 @@ import (
 	"URLShortener/pkg/cache"
 	"URLShortener/pkg/database"
 	"github.com/gin-gonic/gin"
+	"html/template"
 	"log"
+	"net/http"
 )
 
-func InitRouter(db database.DB, cache cache.Redis) *gin.Engine {
+func InitRouter(db *database.DB, cache *cache.Redis) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
+	r.Static("/templates/assets", "./templates/assets/")
+	//check
+
+	templates := template.Must(template.ParseGlob("templates/views/*"))
+	r.SetHTMLTemplate(templates)
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+
+	// curl http://localhost:8080/ping
 	r.GET("/ping", func(c *gin.Context) {
 		log.Println("pong")
 		c.JSON(200, gin.H{
@@ -22,16 +31,29 @@ func InitRouter(db database.DB, cache cache.Redis) *gin.Engine {
 
 	// Initialize controllers
 	urlController := controllers.NewController(db, cache)
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/home")
+	})
 
+	r.GET("/home", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"Title":   "Home Page",
+			"Content": "URL Shortener",
+		})
+	})
+
+	// curl http://localhost:8080/16c6de36
+	r.GET("/:id", urlController.Redirect)
 	// Define API routes
 	api := r.Group("/api")
 	{
 		// curl -X POST -H "Content-Type: application/json" -d '{"originalUrl":"https://www.google.com"}' http://localhost:8080/api/url
-		// curl -X POST -H "Content-Type: application/json" -d '{"originalUrl":"https://www.google.com", "alias":"testingurl"}' http://localhost:8080/api/url
-		api.POST("url", urlController.Create)
+		// curl -X POST -H "Content-Type: application/json" -d '{"originalUrl":"https://www.google.com", "alias":"testingurl"}' http://localhost:8080/api/shorten
+		api.POST("shorten", urlController.Create)
+
+		// curl -X DELETE http://localhost:8080/api/url/16c6de36
 		api.DELETE("url/:id", urlController.Delete)
 	}
-	// curl http://localhost:8080/16c6de36
-	r.GET("/:id", urlController.Redirect)
+
 	return r
 }
