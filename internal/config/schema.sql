a81ec3e05e85ea393de98095378b26ef06fe2e60
An Nguyen <ducan172002@gmail.com>
2023-03-24 00:52:36 +0700

    change type column original_url to longtext

--- internal/config/schema.sql
+++ internal/config/schema.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE IF NOT EXISTS `url_shortener` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 USE `url_shortener`;
 -- Path: internal\config\schema.sql
-CREATE TABLE `shortened_urls` (
+CREATE TABLE IF NOT EXISTS `shortened_urls` (
   `id` char(8) NOT NULL,
-  `original_url` varchar(255) NOT NULL,
+  `original_url` LONGTEXT NOT NULL,
   `short_url` varchar(255) NOT NULL,
 #     `clicks` int(11) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` char(10) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
