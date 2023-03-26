bfede7a3cd81863abd2c193e14c04156e0a03377
An Nguyen <ducan172002@gmail.com>
2023-03-27 02:05:41 +0700

    add and change evironment variable

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,7 +21,7 @@ func LoadConfig(path string) (*Config, error) {
 
 	mysqlCfg := mysql.Config{
 		User:      os.Getenv("MYSQL_USER"),
-		Passwd:    os.Getenv("MYSQL_PASSWORD"),
+		Passwd:    os.Getenv("MYSQL_ROOT_PASSWORD"),
 		Net:       "tcp",
 		Addr:      fmt.Sprintf("%s:%s", os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT")),
 		DBName:    os.Getenv("MYSQL_DATABASE"),
